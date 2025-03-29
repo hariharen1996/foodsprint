3095e732c79199c176105949e3898654f85dfe98
hariharen96 <hariharen1996@gmail.com>
2025-03-29 10:14:38 +0530

    feat:create custom user model with email,phone,address fields

--- users/models.py
+++ users/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
 
 # Create your models here.
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=11,blank=True,null=True)
+    address = models.TextField(blank=True,null=True)
+
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    def __str__(self):
+        return self.email
+
